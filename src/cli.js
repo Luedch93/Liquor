33a2ffaa50380ae41dbc02d69a8075916a2aaf18
Luis Chavez <luischavezcald@gmail.com>
2019-09-16 22:49:21 -0400

    Added first options

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,5 @@
 const arg = require('arg');
+const inquirer = require('inquirer')
 
 function parseArgs(rawArgs) {
     const args = arg(
@@ -15,9 +16,47 @@ function parseArgs(rawArgs) {
     };
 }
 
+async function promptForMissingArgs(options) {
+    const defaultConnection = 'mongodb://localhost:27017/'
+    const defaultTemplate = 'Javascript'
+    const questions = []
+    if (!options.template) {
+        questions.push({
+            type: 'list',
+            name: 'template',
+            message: 'Please choose wich project template to use',
+            choices: ['Javascript', 'Typescript'],
+            default: defaultTemplate
+        })
+    }
+
+    if (options.completeDbURL) {
+        questions.push({
+            type: 'input',
+            name: 'connString',
+            message: 'Please type the connection string of your mongoDB',
+            default: defaultConnection
+        })
+    } else {
+        questions.push({
+            type: 'input',
+            name: 'databaseName',
+            message: 'Please type the name of your database',
+            default: 'LiquorDB'
+        })
+    }
+    const answers = await inquirer.prompt(questions)
+    return {
+        ...options,
+        template: options.template || answers.template,
+        connectionString: answers.connString || defaultConnection + answers.databaseName
+    }
+}
+
 module.exports = {
-    cli(args) {
+    async cli(args) {
         let options = parseArgs(args);
+        options = await promptForMissingArgs(options);
         console.log(options);
     }
 }
